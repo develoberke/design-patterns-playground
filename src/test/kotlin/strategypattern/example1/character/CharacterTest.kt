b6f7e9c2ee8af46f80fb9b5db3a2a82cedcf4c69
Berke Yıldırım <batuhanberke.yildirim@gmail.com>
2024-08-31 15:20:26 +0300

    test refactor

--- src/test/kotlin/strategypattern/example1/character/CharacterTest.kt
+++ src/test/kotlin/strategypattern/example1/character/CharacterTest.kt
@@ -3,6 +3,8 @@ package strategypattern.example1.character
 import com.develoberke.strategypattern.example1.character.Knight
 import com.develoberke.strategypattern.example1.weapon.AxeBehaviour
 import com.develoberke.strategypattern.example1.weapon.SwordBehaviour
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.io.ByteArrayOutputStream
@@ -11,14 +13,24 @@ import kotlin.test.assertEquals
 
 @ExtendWith()
 class CharacterTest {
+    private lateinit var outputStream: ByteArrayOutputStream
+    private lateinit var originalOut: PrintStream
+
+    @BeforeEach
+    fun setUp() {
+        outputStream = ByteArrayOutputStream()
+        originalOut = System.out
+        System.setOut(PrintStream(outputStream))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(originalOut)
+    }
 
     @Test
     fun `it should fight with the weapon`() {
         // given
-        val outputStream = ByteArrayOutputStream()
-        val originalOut = System.out
-        System.setOut(PrintStream(outputStream))
-
         val swordBehaviour = SwordBehaviour()
         val knight = Knight(swordBehaviour)
 
@@ -28,19 +40,14 @@ class CharacterTest {
         // then
         val result = outputStream.toString().trim()
         assertEquals(result, "Knight is fighting\r\nSwinging a sword")
-
-        System.setOut(originalOut)
     }
 
     @Test
     fun `it should change the weapon and fight with the new weapon`() {
         // given
-        val outputStream = ByteArrayOutputStream()
-        val originalOut = System.out
-        System.setOut(PrintStream(outputStream))
-
         val swordBehaviour = SwordBehaviour()
         val axeBehaviour = AxeBehaviour()
+
         val knight = Knight(swordBehaviour)
 
         // when
@@ -50,7 +57,5 @@ class CharacterTest {
         // then
         val result = outputStream.toString().trim()
         assertEquals(result, "Knight is fighting\r\nChopping with an axe")
-
-        System.setOut(originalOut)
     }
 }
